df59f40a34d0988bd88948fd145c4e321cdf0833
Siva Jasthi <siva.jasthi@metrostate.edu>
2020-04-16 12:27:23 -0500

    fixing the database name

--- db_configuration.php
+++ db_configuration.php
@@ -1,7 +1,7 @@
 <?php
 
 DEFINE('DATABASE_HOST', 'localhost');
-DEFINE('DATABASE_DATABASE', 'abcd_db');
+DEFINE('DATABASE_DATABASE', 'abcdishes_db');
 DEFINE('DATABASE_USER', 'root');
 DEFINE('DATABASE_PASSWORD', '');
 
--- db_credentials.php
+++ db_credentials.php
@@ -1,7 +1,7 @@
 <?php
 
 DEFINE('DB_SERVER', 'localhost');
-DEFINE('DB_NAME', 'abcd_db');
+DEFINE('DB_NAME', 'abcdishes_db');
 DEFINE('DB_USER', 'root');
 DEFINE('DB_PASS', '');
 
